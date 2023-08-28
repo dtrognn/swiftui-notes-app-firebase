666e3873994ad869e8a98dbdf2d4087e0fba7b16
dtrognn <dtrognn@gmail.com>
2023-08-28 20:53:38 +0700

    init project

---
+++ NoteAppFirebase/NoteAppFirebase/NoteAppFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppFirebaseApp.swift
+//  NoteAppFirebase
+//
+//  Created by dtrognn on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
